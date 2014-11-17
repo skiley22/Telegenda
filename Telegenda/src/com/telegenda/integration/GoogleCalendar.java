531f51e2c33b5434a2a0d7095fa44f211b8d99ff
Steven Kiley <skiley22@gmail.com>
2014-11-16 23:46:12 -0500

    Don't think the CRON job will work but I'll figure it out.

--- Telegenda/src/com/telegenda/integration/GoogleCalendar.java
+++ Telegenda/src/com/telegenda/integration/GoogleCalendar.java
@@ -1,67 +1,36 @@
 package com.telegenda.integration;
 
-import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.EventAttendee;
 import com.google.api.services.calendar.model.EventDateTime;
 import com.telegenda.business.Listing;
-import com.telegenda.business.Program;
 
 public class GoogleCalendar 
 {
-	private static final String APPLICATION_NAME = "Telegenda";
-	private static final String SERVICE_ACCOUNT_EMAIL = "769449426385-vik1epa1ak3vj9qnfla6oqam7jjqm615@developer.gserviceaccount.com";
-	String privateKeyPw= "notasecret";
-	private static final String MASTER_ACCOUNT = "skiley22@gmail.com";
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 	private static final TimeZone EST = TimeZone.getTimeZone("America/New_York");
-	private static HttpTransport httpTransport;
-	private String summary, description, vendorCalendarId;
+	private String summary, description;
 	private EventDateTime startTime, endTime;
 	private ArrayList<EventAttendee> attendees;
 	private Calendar service;
 	
-	public GoogleCalendar() throws GeneralSecurityException, IOException
+	
+	public GoogleCalendar(Calendar c) throws GeneralSecurityException, IOException
 	{
-		httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-		GoogleCredential credential = new GoogleCredential.Builder()
-				.setTransport(httpTransport)
-				.setJsonFactory(JSON_FACTORY)
-				.setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
-				.setServiceAccountScopes(
-						Collections.singleton(CalendarScopes.CALENDAR))
-				.setServiceAccountPrivateKeyFromP12File(new File("key.p12"))
-				.build();
-
-		service = new Calendar.Builder(httpTransport, JSON_FACTORY,
-				credential).setApplicationName(APPLICATION_NAME).build();
+		service = c;
 	}	
-	public String createEvent(Listing l) throws GeneralSecurityException,
-	IOException {
+	public String createEvent(Listing l) throws GeneralSecurityException, IOException
+	{
 		summary = l.getProgram().getTitle();
 		description = l.getProgram().getTitle() + " on Channel " + l.getChannel().getNumber();
 		startTime = new EventDateTime().setDateTime(new DateTime(l.getProgram().getStartTime(), EST));
 		endTime = new EventDateTime().setDateTime(new DateTime(l.getProgram().getEndTime(), EST));
-		
-		System.out.println("Start time: " + startTime.toPrettyString() + " End time: " + l.getProgram().getEndTime().getTime());
-		
-		attendees = new ArrayList<EventAttendee>();
-		attendees.add(new EventAttendee().setEmail("skiley22@gmail.com"));
 		
 		Event event = new Event();
 		event.setSummary(summary);
@@ -70,8 +39,8 @@ public class GoogleCalendar
 		event.setEnd(endTime);
 		event.setAttendees(attendees);
 				
-		Event e = service.events().insert("f71jkiedt0pp7chdik16pe7teo@group.calendar.google.com", event).execute();
+		Event e = service.events().insert(service.calendarList().list().execute().getItems().get(0).getId(), event).execute();
 		
 		return "New event: " + e.getId() + "Title: " + e.getSummary();
-		}
+	}
 }
