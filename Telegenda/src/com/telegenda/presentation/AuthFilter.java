00a2ba001231fe0b5ac53a7fc2b489e390cb013f
Steve Kiley <skiley22@gmail.com>
2015-07-10 23:35:32 -0400

    Working on implementing a filter to authenticate static HTML requests

---
+++ Telegenda/src/com/telegenda/presentation/AuthFilter.java
@@ -0,0 +1,31 @@
+package com.telegenda.presentation;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class AuthFilter implements Filter 
+{
+
+	@Override
+	public void destroy() {return;}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException 
+	{
+        String url = ((HttpServletRequest) request).getRequestURI();
+        if (url.endsWith(".js") || url.endsWith(".css") || url.endsWith(".html") || url.endsWith(".htm")) {
+            chain.doFilter(request, response);
+        }
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {return;}
+
+}
